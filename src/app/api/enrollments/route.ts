e67d868136cb20c28a947b265417dc5eba31ae7a
Panita Donmuang <panipani.d903@gmail.com>
2024-09-19 15:07:27 +0700

    eiei

--- src/app/api/enrollments/route.ts
+++ src/app/api/enrollments/route.ts
@@ -1,43 +1,51 @@
 import { DB } from "@lib/DB";
 import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
 import { headers } from "next/headers";
-import  jwt from "jsonwebtoken"
-import {Payload} from "@lib/DB"
+import { Payload } from "@lib/DB";
 
 export const GET = async (request: NextRequest) => {
+  const rawAuthHeader = headers().get("authorization");
 
-  //extract token
-  const rawAutHeader = headers().get('autorization');
-  //const token = rawAutHeader?.split(" ")[1];
-
-  if (!rawAutHeader|| !rawAutHeader.startsWith("Bearer")) {
-    return NextResponse.json({
-      ok: false,
-      messege: "Missing autorization header"
-  })
+  if (!rawAuthHeader || !rawAuthHeader.startsWith("Bearer ")) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Authorization header is required",
+      },
+      { status: 401 }
+    );
   }
 
-  const token = rawAutHeader?.split(' ')[1];
-  const secret = process.env.JWT_SECRET || "This is another secret";
+  const token = rawAuthHeader.split(" ")[1];
 
+  const secret = process.env.JWT_SECRET || "This is my special secret";
   let studentId = null;
-  try{
-  const payload = jwt.verify(token, secret);
-  console.log(payload);
-  studentId = (<Payload>payload).studentId;
+
+  //preparing "role" variable for reading role information from token
+  let role = null;
+
+  try {
+    const payload = jwt.verify(token, secret);
+    studentId = (<Payload>payload).studentId;
+
+    //read role information from "payload" here (just one line code!)
+    //role = ...
   } catch {
-    return NextResponse.json({
-      ok: false,
-      messege: "Invalid token"
-    }, {status: 401})
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Invalid token",
+      },
+      { status: 401 }
+    );
   }
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Invalid token",
-  //   },
-  //   { status: 401 }
-  // );
+
+  //Check role here. If user is "ADMIN" show all of the enrollments instead
+  //   return NextResponse.json({
+  //     ok: true,
+  //     enrollments: null //replace null with enrollment data!
+  // }
 
   const courseNoList = [];
   for (const enroll of DB.enrollments) {
@@ -52,6 +60,51 @@ export const GET = async (request: NextRequest) => {
 };
 
 export const POST = async (request: NextRequest) => {
+  const rawAuthHeader = headers().get("authorization");
+
+  if (!rawAuthHeader || !rawAuthHeader.startsWith("Bearer ")) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Authorization header is required",
+      },
+      { status: 401 }
+    );
+  }
+
+  const token = rawAuthHeader.split(" ")[1];
+
+  const secret = process.env.JWT_SECRET || "This is my special secret";
+  let studentId = null;
+
+  //preparing "role" variable for reading role information from token
+  let role = null;
+
+  try {
+    const payload = jwt.verify(token, secret);
+    studentId = (<Payload>payload).studentId;
+
+    //read role information from "payload" here (just one line code!)
+    //role = ...
+  } catch {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Invalid token",
+      },
+      { status: 401 }
+    );
+  }
+
+  //if role is "ADMIN", send the following response
+  // return NextResponse.json(
+  //   {
+  //     ok: true,
+  //     message: "Only Student can access this API route",
+  //   },
+  //   { status: 403 }
+  // );
+
   //read body request
   const body = await request.json();
   const { courseNo } = body;
@@ -101,5 +154,55 @@ export const POST = async (request: NextRequest) => {
   return NextResponse.json({
     ok: true,
     message: "You has enrolled a course successfully",
+  });
+};
+
+export const DELETE = async (request: NextRequest) => {
+  //check token
+  //verify token and get "studentId" and "role" information here
+  let studentId = null;
+  let role = null;
+
+  //if role is "ADMIN", send the following response
+  // return NextResponse.json(
+  //   {
+  //     ok: true,
+  //     message: "Only Student can access this API route",
+  //   },
+  //   { status: 403 }
+  // );
+
+  //get courseNo from body and validate it
+  const body = await request.json();
+  const { courseNo } = body;
+  if (typeof courseNo !== "string" || courseNo.length !== 6) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "courseNo must contain 6 characters",
+      },
+      { status: 400 }
+    );
+  }
+
+  const foundIndex = DB.enrollments.findIndex(
+    (x) => x.studentId === studentId && x.courseNo === courseNo
+  );
+  if (foundIndex === -1) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message:
+          "You cannot drop from this course. You have not enrolled it yet!",
+      },
+      { status: 404 }
+    );
+  }
+
+  DB.enrollments.splice(foundIndex, 1);
+
+  return NextResponse.json({
+    ok: true,
+    message: "You has dropped from this course. See you next semester.",
   });
 };
